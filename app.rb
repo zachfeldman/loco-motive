80ec76971bffee7ed183c54efde8688f65c17700
Zach Feldman <zach@nycda.com>
2015-08-05 11:45:44 -0400

    Working.

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require "sinatra"
+require "net/http"
+require "uri"
+require "JSON"
+
+def get_results
+  JSON.parse(Net::HTTP.get(URI.parse("http://www.amtrakconnect.com/nomadtojson.php")))
+end
 
 get "/" do
+  @results = get_results
   erb :dashboard
+end
+
+get "/results" do
+  get_results.to_json
 end
